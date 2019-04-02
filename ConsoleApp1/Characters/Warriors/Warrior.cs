bcf4f61e8c30ac588926da82a94aa9a634730d6b
Dejan Stjepanovic <Dejan.Stjepanovic@ey.com>
2019-04-02 16:44:43 -0500

    first commit

---
+++ ConsoleApp1/Characters/Warriors/Warrior.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+
+namespace Characters.Warriors
+
+{
+    public class Warrior
+    {
+        private int height;
+        private int weight;
+        private string name;
+        private int age;
+
+
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                height = value;
+            }
+        }
+        public int Weight { get; set; }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value >= 18 && value <= 45)
+                {
+                    age = value;
+                }
+                else
+                {
+                    //Why this not work?
+                    throw new ArgumentOutOfRangeException(string.Empty, $@"The warrior {name} is too young to fight");
+                    //Console.WriteLine($@"The warrior {name} is too young to fight");
+                }
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        //constructor
+        public Warrior(int height, int weight, string name)
+        {
+            Height = height;
+            Weight = weight;
+            Name = name;
+
+        }
+
+        public void Greetings(Warrior warrior)
+        {
+            Console.WriteLine($@"Hello, {warrior.Name}!");
+        }
+    }
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Characters.Warriors;
+
+
+    public class EntryPoint
+    {
+        static void Main()
+        {
+            Warrior theGoodGuy = new Warrior(190, 80, "Good Guy");
+        theGoodGuy.Age = 25;
+            //theGoodGuy.Height = 190;
+            //theGoodGuy.Weight = 80;
+            //theGoodGuy.Name = "Good Guy";
+
+            Warrior theBadGuy = new Warrior(170, 70, "Bad Guy");
+        theBadGuy.Age = 15;
+            //theBadGuy.Height = 170;
+            //theBadGuy.Weight = 70;
+            //theBadGuy.Name = "Bad Guy";
+
+            theGoodGuy.Greetings(theBadGuy);
+            theBadGuy.Greetings(theGoodGuy);
+
+        Console.WriteLine($@"The warrior {theGoodGuy.Name} is {theGoodGuy.Age}");
+        Console.WriteLine($@"The warrior {theBadGuy.Name} is {theBadGuy.Age}");
+
+        Console.WriteLine(theBadGuy.Height);
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+        try
+        {
+            theGoodGuy.Age = 25;
+            theBadGuy.Age = 15;
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    }
+
+
